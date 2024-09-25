6bc6260de7c6355eae89903086819e715c9ef7a5
Young Min Kang <122731556+Baguette-bbang@users.noreply.github.com>
2024-09-25 15:49:20 +0900

    Revert "[FIX] 스케줄 관련 컨트롤러 및 DTO 오류 처리 및 개선"

--- src/modules/schedules/dto/data-range-schedule.dto.ts
+++ src/modules/schedules/dto/data-range-schedule.dto.ts
@@ -1,8 +1,17 @@
-import { CreateScheduleDto } from './create-schedule.dto';
-import { PickType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class DateRangeDto extends PickType(CreateScheduleDto, [
-  'startDate',
-  'endDate',
-  'userId',
-] as const) {}
+export class DateRangeDto {
+  @ApiProperty({ description: '시작 날짜', example: '2023-09-01' })
+  @IsNotEmpty()
+  @IsDate()
+  @Type(() => Date)
+  startDate: Date;
+
+  @ApiProperty({ description: '종료 날짜', example: '2023-09-30' })
+  @IsNotEmpty()
+  @IsDate()
+  @Type(() => Date)
+  endDate: Date;
+}
