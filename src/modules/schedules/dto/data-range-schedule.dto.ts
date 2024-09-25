f253ba01353b735658a7e4be4c6661b8599917b5
Baguette-bbang <dudals9701@naver.com>
2024-09-25 23:34:22 +0900

    docs: 특정 날짜 범위의 일정 조회 기능 스웨거 문서 쿼리 업데이트

--- src/modules/schedules/dto/data-range-schedule.dto.ts
+++ src/modules/schedules/dto/data-range-schedule.dto.ts
@@ -1,8 +1,27 @@
 import { CreateScheduleDto } from './create-schedule.dto';
-import { PickType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class DateRangeDto extends PickType(CreateScheduleDto, [
   'startDate',
   'endDate',
   'userId',
-] as const) {}
+] as const) {
+  @ApiProperty({
+    description: '조회 시작 날짜',
+    example: '2024-09-01',
+  })
+  startDate: Date;
+
+  @ApiProperty({
+    description: '조회 종료 날짜',
+    example: '2024-09-30',
+  })
+  endDate: Date;
+
+  @ApiProperty({
+    description: '사용자 ID',
+    example: 1,
+  })
+  userId: number;
+}
