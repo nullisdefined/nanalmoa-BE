469a7d0da6f4a18709e338a77f482181ef977d87
nullisdefined <jaeuu.dev@gmail.com>
2024-10-03 23:18:08 +0900

    refactor: 인증번호 -> 인증코드 문자열 수정

--- src/auth/coolsms.service.ts
+++ src/auth/coolsms.service.ts
@@ -21,9 +21,9 @@ export class CoolSmsService {
       const result = await this.messageService.sendOne({
         to: phoneNumber,
         from: this.configService.get<string>('COOLSMS_SENDER_PHONE_NUMBER'),
-        text: `[나날모아] 본인확인 인증번호
+        text: `[나날모아] 본인확인 인증코드
 [${verificationCode}]를 화면에 입력해주세요.
-이 번호는 ${expirationMinutes}분동안 유효합니다.`,
+이 코드는 ${expirationMinutes}분동안 유효합니다.`,
       });
 
       console.log('SMS send result:', result);
