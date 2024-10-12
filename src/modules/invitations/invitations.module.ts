1090bd82da41cdbecd2c2c37ec4d2768c7f11a21
nullisdefined <jaeuu.dev@gmail.com>
2024-10-12 17:03:34 +0900

    docs: API 문서 업데이트

--- src/modules/invitations/invitations.module.ts
+++ src/modules/invitations/invitations.module.ts
@@ -5,10 +5,12 @@ import { InvitationsService } from './invitations.service';
 import { GroupInvitation } from 'src/entities/group-invitation.entity';
 import { ManagerInvitation } from 'src/entities/manager-invitation.entity';
 import { User } from 'src/entities/user.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([GroupInvitation, ManagerInvitation, User]),
+    UsersModule,
   ],
   controllers: [InvitationsController],
   providers: [InvitationsService],
