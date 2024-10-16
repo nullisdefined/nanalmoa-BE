6e1f3999925521db55663b009bbc59f05b11c4e6
Baguette-bbang <dudals9701@naver.com>
2024-10-17 03:17:44 +0900

    [REFACTOR] 초대 로그 응답 시 그룹명과 유저명도 반환

--- src/modules/group/dto/response-invitation.dto.ts
+++ src/modules/group/dto/response-invitation.dto.ts
@@ -7,12 +7,21 @@ export class RespondToInvitationDto {
 
   @ApiProperty({ description: '그룹 ID' })
   groupId: number;
+  @ApiProperty({
+    description: '그룹명',
+    example: '바게트빵을 좋아하는 사람들의 모임',
+  })
+  groupName: string;
 
   @ApiProperty({ description: '초대한 사용자의 UUID' })
   inviterUuid: string;
+  @ApiProperty({ description: '초대한 사용자 이름', example: '홍길동' })
+  inviterName: string;
 
   @ApiProperty({ description: '초대받은 사용자의 UUID' })
   inviteeUuid: string;
+  @ApiProperty({ description: '초대받은 사용자 이름', example: '김철수' })
+  inviteeName: string;
 
   @ApiProperty({ description: '초대 상태', enum: InvitationStatus })
   status: InvitationStatus;
